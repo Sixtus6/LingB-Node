d0f46645fdaec7da18c3e6989cd7076c167da6b4
sixtus6 <xystuseton@gmail.com>
2023-10-26 00:55:50 +0100

    refactored changes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const http = require("http");
-const Snooker = require("./snooker.js/index.js");
-const Chess = require("./chess");
-const Dice = require("./dice");
-const Ludo = require("./ludo");
-const Scrabble = require("./scrabble");
+// const Snooker = require("./snooker.js/index.js");
+// const Chess = require("./chess");
+// const Dice = require("./dice");
+// const Ludo = require("./ludo");
+// const Scrabble = require("./scrabble");
 
-const TicTacToe = require("./tic_tac_toe");
+// const TicTacToe = require("./tic_tac_toe");
 
 class Socket {
 
@@ -14,25 +14,19 @@ class Socket {
     }
 
     initSocket(app, callback) {
-
         this.app = app;
-
         this.server = http.createServer(this.app);
-
         this.io = require("socket.io")(this.server);
-
         this.init = true;
-
         this.io.on("connection", (socket) => {
-            this.socket = socket;
-            TicTacToe.gamefunction(this.socket, this.io);
-            Dice.gamefunction(this.socket, this.io);
-            Chess.gamefunction(this.socket, this.io);
-            Scrabble.gamefunction(this.socket, this.io);
-            Ludo.gamefuntion(this.socket, this.io);
-            Snooker.gamefuntion(this.socket, this.io);
+            // this.socket = socket;
+            // TicTacToe.gamefunction(this.socket, this.io);
+            // Dice.gamefunction(this.socket, this.io);
+            // Chess.gamefunction(this.socket, this.io);
+            // Scrabble.gamefunction(this.socket, this.io);
+            // Ludo.gamefuntion(this.socket, this.io);
+            // Snooker.gamefuntion(this.socket, this.io);
         });
-
         callback(this.server);
     }
 }
