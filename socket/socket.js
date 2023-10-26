521421e9c8170fdd6809b0ac85d8ed41da809a66
sixtus6 <xystuseton@gmail.com>
2023-10-26 06:45:10 +0100

    cnofigure redis server config

--- socket/socket.js
+++ socket/socket.js
@@ -1,9 +1,22 @@
 const http = require("http");
-
+const redis = require("redis");
+var redisClient = redis.createClient({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+});
+var isconnected = false;
 class Socket {
 
     constructor() {
         this.init = false;
+        if (!isconnected) {
+
+            (async () => {
+                await redisClient.connect();
+            })();
+            isconnected = true;
+            console.log(`connected successfully to Redis`);
+        } else { }
     }
 
     initSocket(app, callback) {
@@ -12,7 +25,7 @@ class Socket {
         this.io = require("socket.io")(this.server);
         this.init = true;
         this.io.on("connection", (socket) => {
-            // this.socket = socket;
+            this.socket = socket;
             // TicTacToe.gamefunction(this.socket, this.io);
             // Dice.gamefunction(this.socket, this.io);
             // Chess.gamefunction(this.socket, this.io);
