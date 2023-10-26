6347a980ce1f1cad989789465c86e50e1bb1b6f6
sixtus6 <xystuseton@gmail.com>
2023-10-26 09:00:07 +0100

    saved refactored changes

--- chatroom/chatroom.js
+++ chatroom/chatroom.js
@@ -4,10 +4,19 @@ const md5 = require("md5");
 class ChatRoom{
 
     static initChatRoom(socket, io) {
-
+       // console.log(`User  ${socket.id}`);
         socket.on("create-room", async ({ username, language }) => {
+            
             console.log(username, language)
-         })
+            console.log(`User connected created a room: ${socket.id}`);
+
+        })
+        
+        socket.on("disconnect", () => {
+            // When a user disconnects
+            console.log(`User disconnected from room: ${socket.id}`);
+          //  delete onlineUsers[socket.id];
+          });
 
     }
 
