4615800c1e183b221bf38a61f0bc948ccf2b9d39
sixtus6 <xystuseton@gmail.com>
2023-10-27 17:53:01 +0100

    refactored changes

--- services/translator.js
+++ services/translator.js
@@ -1,7 +1,7 @@
+require('dotenv').config(); 
 const { Translate } = require('@google-cloud/translate');
-require('dotenv').config();
-const apiKey = process.env.DATABASE_HOST;
-
-const translate = new Translate({
-    key: apiKey, // Provide your API key
-  });
+const apiKey = process.REDIS_HOST;
+console.log(apiKey)
+// const translate = new Translate({
+//     key: apiKey, // Provide your API key
+//   });
