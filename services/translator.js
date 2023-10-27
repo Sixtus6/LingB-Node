3c3626385abbd644a5eba5965c1683481310d50a
sixtus6 <xystuseton@gmail.com>
2023-10-28 00:32:42 +0100

    tested transalation funtion it working

--- services/translator.js
+++ services/translator.js
@@ -1,34 +1,34 @@
 const axios = require('axios');
 const { googleapi } = require('../key');
 const apiKey = googleapi; // Replace with your actual API key
-const text = 'Hello, World!';
-const targetLanguage = 'es';
-
+const text = 'good morning!';
+const targetLanguage = 'yo';
+// yo,  ig, ha
 module.exports = {
-    translator : async function translateText() {
+    translator: async function (message, ) {
         try {
-          const url = 'https://translation.googleapis.com/language/translate/v2';
-          const data = {
-            q: text,
-            target: targetLanguage,
-          };
-      
-          const response = await axios.post(url, data, {
-            params: {
-              key: apiKey,
-            },
-          });
-      
-          if (response.status === 200) {
-            const translation = response.data.data.translations[0].translatedText;
-            console.log(`Text: ${text}`);
-            console.log(`Translation: ${translation}`);
-          } else {
-            console.error('Error translating text:', response.statusText);
-          }
+            const url = 'https://translation.googleapis.com/language/translate/v2';
+            const data = {
+                q: text,
+                target: targetLanguage,
+            };
+
+            const response = await axios.post(url, data, {
+                params: {
+                    key: apiKey,
+                },
+            });
+
+            if (response.status === 200) {
+                const translation = response.data.data.translations[0].translatedText;
+                console.log(`Text: ${text}`);
+                console.log(`Translation: ${translation}`);
+            } else {
+                console.error('Error translating text:', response.statusText);
+            }
         } catch (err) {
-          console.error('Error translating text:', err);
+            console.error('Error translating text:', err);
         }
-      }
+    }
 }
 
